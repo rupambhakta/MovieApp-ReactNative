658df1db55d57408718785e032dafb9c8b6cec0a
rupambhakta <rupambhakta2020@gmail.com>
2025-08-23 00:02:01 +0530

    TMDB Api Implimented

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,23 @@
-import { icons } from '@/constants/icons'
-import React from 'react'
-import { Image, TextInput, View } from 'react-native'
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
 
-const SearchBar = () => {
+interface Props {
+    placeholder: string;
+    onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, onPress }: Props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-2'>
             <Image source={icons.search} className='size-5' resizeMode='contain'
                 tintColor='#ab8bff' />
-            <TextInput onPress={() => { }}
-            placeholder='Search'
-            value=''
-            onChangeText={()=>{}}
-            placeholderTextColor='#a8b5db'
-            className='flex-1 ml-2 text-white' />
+            <TextInput onPress={onPress}
+                placeholder={placeholder}
+                value=''
+                onChangeText={() => { }}
+                placeholderTextColor='#a8b5db'
+                className='flex-1 ml-2 text-white' />
 
         </View>
     )
